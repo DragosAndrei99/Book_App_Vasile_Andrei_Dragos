d691e1a57622d4538abbf7aee98497530b42a709
DragosAndrei99 <vdragos1999@yahoo.com>
2024-04-09 13:19:34 +0300

    Implement search in book list and author list

--- Book_App_Vasile_Andrei_Dragos/Views/Author/ListAuthors.xaml.cs
+++ Book_App_Vasile_Andrei_Dragos/Views/Author/ListAuthors.xaml.cs
@@ -1,12 +1,16 @@
-﻿using Book_App_Vasile_Andrei_Dragos.ViewModels.Author;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using Book_App_Vasile_Andrei_Dragos.Models.Author;
+using Book_App_Vasile_Andrei_Dragos.ViewModels.Author;
 
 
 namespace Book_App_Vasile_Andrei_Dragos.Views.Author
 {
     public partial class ListAuthors : Page
     {
+        private IEnumerable<AuthorDTO> _authorsList = new List<AuthorDTO>();
         public ListAuthors()
         {
             InitializeComponent();
@@ -17,6 +21,15 @@ namespace Book_App_Vasile_Andrei_Dragos.Views.Author
         {
             string selectedAuthorId = ((Button)sender).Tag.ToString();
             this.NavigationService.Navigate(new Author(selectedAuthorId));
+        }
+
+        private void HandleKeyUpSearch(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (_authorsList.Count() == 0)
+            {
+                _authorsList = ((IEnumerable<AuthorDTO>)listAuthorsDataGrid.ItemsSource).ToList();
+            }
+            listAuthorsDataGrid.ItemsSource = _authorsList.Where(author => author.LastName.ToLower().Contains(searchLastNameTextBox.Text.ToLower()));
         }
     }
 }
--- Book_App_Vasile_Andrei_Dragos/Views/Book/ListBooks.xaml.cs
+++ Book_App_Vasile_Andrei_Dragos/Views/Book/ListBooks.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
+using Book_App_Vasile_Andrei_Dragos.Models.Book;
 using Book_App_Vasile_Andrei_Dragos.ViewModels.Book;
 
 
@@ -8,6 +11,7 @@ namespace Book_App_Vasile_Andrei_Dragos.Views.Book
 
     public partial class ListBooks : Page
     {
+        private IEnumerable<BookWithAuthors> _bookWithAuthorsList = new List<BookWithAuthors>();   
         public ListBooks()
         {
             InitializeComponent();
@@ -19,5 +23,14 @@ namespace Book_App_Vasile_Andrei_Dragos.Views.Book
             string selectedBookId = ((Button)sender).Tag.ToString();
             this.NavigationService.Navigate(new Book(selectedBookId));
         }
-    }
+
+        private void HandleKeyUpSearch(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if(_bookWithAuthorsList.Count() == 0)
+            {
+                _bookWithAuthorsList = ((IEnumerable<BookWithAuthors>)listBooksDataGrid.ItemsSource).ToList();
+            }
+            listBooksDataGrid.ItemsSource = _bookWithAuthorsList.Where(book => book.Title.ToLower().Contains(searchTitleTextBox.Text.ToLower()) && book.Authors.ToLower().Contains(searchAuthorsTextBox.Text.ToLower()));
+        }
+    }   
 }
